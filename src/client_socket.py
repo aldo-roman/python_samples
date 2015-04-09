01ebec76fba0fdba6e517e743a9a728393033ceb
Aldo Román Nureña <aldo.roman.nurena@gmail.com>
2015-04-09 16:34:21 -0500

    added user input

--- src/client_socket.py
+++ src/client_socket.py
@@ -1,4 +1,5 @@
-from src.classes.socket import Socket
+from sys import stdin
+from src.classes.sockets import Socket
 
 __author__ = 'Aldo Roman Nurena'
 
@@ -8,12 +9,17 @@ def __main__():
 
 
 def client_socket():
-    s = Socket()
+    s = Socket.get_instance()
     s.connect(('localhost',1234))
+    print "Connected to server."
+    print "Write some text: (line by line)"
 
-    s.send("I'm client socket!")
-    response = s.recv(64)
-    print(response)
-    s.close()
+    while True:
+        line = stdin.readline()
+        # s.send(line.length)
+        s.send(line)
+        response = s.recv(64)
+        print(response)
+        s.close()
 
 __main__()
